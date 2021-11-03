f70ff4febd89e142a87fec83b4d5d5cddf739d9b
Wyatt Saint <wmsaint17@gmail.com>
2021-11-02 22:43:48 -0400

    Added mariaDB functionality to add and delete tasks

--- src/toDoList/toDoList.java
+++ src/toDoList/toDoList.java
@@ -10,11 +10,20 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Scanner;
+import java.sql.*;
 
 public class toDoList {
+	
+	static final String JDBC_DRIVER = "org.mariadb.jdbc.Driver";
+    static final String DB_URL = "jdbc:mariadb://192.168.100.174/db";
 
-	public static void main(String[] args) throws IOException {
+    //  Database credentials
+    static final String USER = "root";
+    static final String PASS = "root";
 
+
+	public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException {
+		
 		BufferedReader bchoice = new BufferedReader(new FileReader("taskList.txt"));
 
 		while (bchoice.readLine() != null) {
@@ -34,7 +43,7 @@ public class toDoList {
 		
 	}
 
-	public static void addTaskAndStore() throws IOException {
+	public static void addTaskAndStore() throws IOException, ClassNotFoundException, SQLException {
 		System.out.println("Enter task: ");
 		Scanner scanner = new Scanner(System.in);
 		String task = scanner.nextLine();
@@ -43,6 +52,7 @@ public class toDoList {
 		bw.newLine();
 		bw.flush();
 		printTodoListToTerminal();
+		databaseInsert(task);
 	}
 
 	public static void printTodoListToTerminal() throws IOException {
@@ -54,7 +64,7 @@ public class toDoList {
 		}
 	}
 
-	public static void deleteTask() throws IOException {
+	public static void deleteTask() throws IOException, ClassNotFoundException, SQLException {
 		printTodoListToTerminal();
 		BufferedReader br = new BufferedReader(new FileReader("taskList.txt"));
 		BufferedWriter bw = new BufferedWriter(new FileWriter("temp.txt"));
@@ -72,6 +82,7 @@ public class toDoList {
 				System.out.println("Deleted " + line);
 			}
 		}
+		databaseDelete(deletedTask);
 	
 	}
 	
@@ -87,6 +98,54 @@ public class toDoList {
 		}
 		is.close();
 		os.close();
+	}
+	
+	static void databaseInsert(String task) throws ClassNotFoundException, SQLException {
+		
+		Connection conn = null;
+        Statement stmt = null;
+            //STEP 2: Register JDBC driver
+        	Class.forName("org.mariadb.jdbc.Driver");
+            
+            //STEP 3: Open a connection
+            System.out.println("Connecting to a selected database...");
+            conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/test", "root", "root");
+            System.out.println("Connected database successfully...");
+
+            //STEP 4: Execute a query
+            System.out.println("Inserting task in given table...");
+            stmt = conn.createStatement();
+
+            String sql = "insert into testtable (task) VALUES " + "('" + task + "')";
+            
+
+            stmt.executeUpdate(sql);
+            System.out.println("Inserted task in given database table...");
+		
+	}
+	
+	static void databaseDelete(String task) throws ClassNotFoundException, SQLException {
+		
+		Connection conn = null;
+        Statement stmt = null;
+            //STEP 2: Register JDBC driver
+        	Class.forName("org.mariadb.jdbc.Driver");
+            
+            //STEP 3: Open a connection
+            System.out.println("Connecting to a selected database...");
+            conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/test", "root", "root");
+            System.out.println("Connected database successfully...");
+
+            //STEP 4: Execute a query
+            System.out.println("Deleting task in given table...");
+            stmt = conn.createStatement();
+
+            String sql = "delete from testtable where task = " + "('" + task + "')";
+            
+
+            stmt.executeUpdate(sql);
+            System.out.println("Deleted task in given database table...");
+		
 	}
 
 }
